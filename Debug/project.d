806dc3d33f73e49dc4f7ca7326e045f9de3a70c8
Ahmed-Ibrahim <eng.ahmedibrahim21@gmail.com>
2021-11-03 00:29:41 +0200

    Debug

---
+++ Debug/project.d
@@ -0,0 +1,12 @@
+project.o project.o: ../project.c ../lcd.h ../std_types.h ../adc.h \
+  ../lm35_sensor.h ../dcmotor.h
+
+../lcd.h:
+
+../std_types.h:
+
+../adc.h:
+
+../lm35_sensor.h:
+
+../dcmotor.h:
